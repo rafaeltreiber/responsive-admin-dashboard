34921d5f395962c0aaf70c20ed8643adaf76e5f0
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2022-04-15 20:50:47 -0300

    finished

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shiping: "Pending",
+  },
+  {
+    productName: "LRVENDER KF102 DRONE",
+    productNumber: "36378",
+    paymentStatus: "Refunded",
+    shiping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shiping: "Pending",
+  },
+  {
+    productName: "Drone With Camera",
+    productNumber: "96996",
+    paymentStatus: "Paid",
+    shiping: "Delivered",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shiping: "Delivered",
+  },
+  {
+    productName: "DJI Air 2S",
+    productNumber: "81475",
+    paymentStatus: "Due",
+    shiping: "Pending",
+  },
+  {
+    productName: "Lozenge Drone",
+    productNumber: "00482",
+    paymentStatus: "Paid",
+    shiping: "Delivered",
+  },
+];
